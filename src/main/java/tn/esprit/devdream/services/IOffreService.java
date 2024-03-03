143c524000e9097b35a95b5d027d001d2d8f9e2b
Ranim Amer <ranim.amer@esprit.tn>
2024-03-03 22:30:13 +0100

    ranimback

---
+++ src/main/java/tn/esprit/devdream/services/IOffreService.java
@@ -0,0 +1,27 @@
+package tn.esprit.devdream.services;
+
+import tn.esprit.devdream.entities.Application;
+import tn.esprit.devdream.entities.Offre;
+import tn.esprit.devdream.entities.User;
+
+import java.util.List;
+
+public interface IOffreService {
+    public List<Offre> retrieveAllOffres();
+    public Offre retrieveOffre(Long id_offre);
+    public Offre addOffre(Offre o);
+    public void removeOffre(Long id_offre);
+    public Offre modifyOffre(Offre o);
+
+    public Application applytooffer(Offre o);
+
+
+
+
+
+    List<Offre> searchOffers(String keywords);
+
+    List<User> getEtudiantsparoffre(Long  id_offre);
+
+    void accepterEtudiant(Long  id_offre, Long id);
+}
