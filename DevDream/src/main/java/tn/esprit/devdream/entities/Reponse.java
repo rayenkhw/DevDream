2a869468cb2eedf462c48043494f8f09a52c60ae
Doritos <dorra.ferah@esprit.tn>
2024-03-04 11:48:35 +0100

    back push

---
+++ DevDream/src/main/java/tn/esprit/devdream/entities/Reponse.java
@@ -0,0 +1,34 @@
+package tn.esprit.devdream.entities;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="Reponse")
+
+public class Reponse implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_reponse")
+    private Long id_reponse;
+
+
+    private String reponse;
+
+    @JsonIgnore
+    @JsonBackReference
+    @OneToOne
+    private Reclamation reclamation;
+
+}
---
+++ DevDream/src/main/java/tn/esprit/devdream/services/ChatServiceImpl.java
@@ -0,0 +1,36 @@
+package tn.esprit.devdream.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import tn.esprit.devdream.entities.Chat;
+import tn.esprit.devdream.repositories.IChatRepository;
+
+import java.util.List;
+@Service
+@AllArgsConstructor
+public class ChatServiceImpl implements IChatService {
+    IChatRepository iChatRepository;
+    @Override
+    public Chat addChat (Chat c) {
+        return iChatRepository.save(c);
+    }
+
+    @Override
+    public Chat modifyChat(Chat chat) {
+        return iChatRepository.save(chat);
+    }
+    @Override
+    public List<Chat> retrieveAllChat() {
+
+        return iChatRepository.findAll();
+    }
+    @Override
+    public Chat retrieveChat(Long idChat) {
+        return iChatRepository.findById(idChat).get();
+    }
+    @Override
+    public void removeChat(Long idChat) {
+        iChatRepository.deleteById(idChat);
+
+    }
+}
