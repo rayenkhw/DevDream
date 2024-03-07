a330a8c8e89e36cc975083e9566d319325ac2581
Ranim Amer <ranim.amer@esprit.tn>
2024-03-07 18:33:26 +0100

    first

--- src/main/java/tn/esprit/devdream/services/IRateService.java
+++
@@ -1,4 +0,0 @@
-package tn.esprit.devdream.services;
-
-public interface IRateService {
-}
--- src/main/java/tn/esprit/devdream/services/RateServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.devdream.services;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-@Service
-@Slf4j
-public class RateServiceImpl implements IRateService{
-}
